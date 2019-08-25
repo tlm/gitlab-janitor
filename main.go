eefd0e7b057fae6d2626ab61e323e399f4cc1a05
Thomas Miller <tmiller@apnic.net>
2019-08-25 18:34:37 +1000

    Cleaning up testing code

--- main.go
+++ main.go
@@ -7,28 +7,9 @@ import (
 	"github.com/tlmiller/janitor/cmd"
 )
 
-//func PruneBackups(res http.ResponseWriter, req *http.Request) {
-//	log.Println("got event")
-//	if b, err := ioutil.ReadAll(req.Body); err == nil {
-//		log.Println(string(b))
-//	}
-//}
-
 func main() {
 	if err := cmd.New().Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	//port := os.Getenv("PORT")
-	//if port == "" {
-	//	port = "8080"
-	//}
-
-	//server := http.Server{
-	//	Addr:    fmt.Sprintf(":%s", port),
-	//	Handler: http.HandlerFunc(PruneBackups),
-	//}
-
-	//log.Printf("starting server on port %s", port)
-	//log.Fatal(server.ListenAndServe())
 }
